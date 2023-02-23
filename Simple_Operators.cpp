509d26d44e33989a0bbe93dfac38a41af55f574b
Cute Newbie <dongyoong5@naver.com>
2023-02-23 14:42:41 +0900

    Add files via upload

---
+++ Simple_Operators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+int main(void)
+
+{
+	int x = 2; // x is variable 
+	std::cout << "Hello World" << std::endl;
+	std::cout << x+2 << std::endl;
+
+	int y = (x > 0) ? 1 : 2;
+	std::cout <<y << std::endl;
+
+
+
+	return 0;
+}
