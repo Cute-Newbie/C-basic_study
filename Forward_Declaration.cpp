afe823f7778e3e67f62404ac847f4371054abe59
Cute Newbie <dongyoong5@naver.com>
2023-02-23 15:06:11 +0900

    Add files via upload

---
+++ Forward_Declaration.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+int adder(int a, int b);//forward declaration
+
+int main(void)
+
+{
+	int x = 2; // x is variable 
+	std::cout << "Hello World" << std::endl;
+	std::cout << x+2 << std::endl;
+
+	int y = (x > 0) ? 1 : 2;
+	std::cout <<y << std::endl;
+
+
+
+	return 0;
+}
+int adder(int a, int b) // definition
+{
+
+	return a + b;
+
+}
